1675dd0de1dc1c4272a3856ab2825ee240e7b620
Bob Oyier <boboyier92@gmail.com>
2023-02-05 13:20:08 +0300

    Completed the Countdown to Midnight lab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  x = 1
+  while int >= x
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+p countdown(5)
+
+def countdown_with_sleep(int, seconds=1)
+  x = 1
+  while int >= x
+	sleep seconds
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+# p countdown_with_sleep(5, 1)
